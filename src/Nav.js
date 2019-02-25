70255ca70140d2bc05af5a3e6518a9becd50c972
miked0331 <mikedragun@gmail.com>
2019-02-25 12:12:42 -0500

    Adding nav flex

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Nav extends Component {
     render() {
       return (
-        <div>
-          This is my Nav
+    <div class="Navbar">
+        <nav class="Navbar__Items">
+        <div class="Navbar__Link Navbar__Link-brand">
+            Website title
         </div>
+        <div class="Navbar__Link">
+            Link
+        </div>
+        <div class="Navbar__Link">
+            Link
+        </div>
+        <div class="Navbar__Link">
+            Link
+        </div>
+        </nav>
+        <nav class="Navbar__Items Navbar__Items--right">
+        <div class="Navbar__Link">
+            Link
+        </div>
+        <div class="Navbar__Link">
+            Link
+        </div>
+        </nav>
+</div>
       );
     }
   }
