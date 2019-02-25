fe0860c80f54585a3944e8fa580a30ecb6354d68
miked0331 <mikedragun@gmail.com>
2019-02-25 15:00:30 -0500

    working Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,35 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-class Nav extends Component {
-    render() {
-      return (
-    <div class="Navbar">
-        <nav class="Navbar__Items">
-        <div class="Navbar__Link Navbar__Link-brand">
-            Website title
-        </div>
-        <div class="Navbar__Link">
-            Link
-        </div>
-        <div class="Navbar__Link">
-            Link
-        </div>
-        <div class="Navbar__Link">
-            Link
-        </div>
-        </nav>
-        <nav class="Navbar__Items Navbar__Items--right">
-        <div class="Navbar__Link">
-            Link
-        </div>
-        <div class="Navbar__Link">
-            Link
-        </div>
-        </nav>
-</div>
-      );
-    }
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
   }
-  
-  export default Nav;
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
