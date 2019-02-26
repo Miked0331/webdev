4a1ef9f3c0b4c0ebad8d006b531e1fe8faf6c3e7
miked0331 <mikedragun@gmail.com>
2019-02-26 13:17:11 -0500

    adding html body image and text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import Html from './Image/html.jpg'
 
 class App extends Component {
   render() {
     return (
-
+<div>
       <div className="wrapper">
         <div className="main-text"> Learn the web development essentials</div>
-        <div className="hr">
+        <div className="hr">  
         </div>
       </div>
 
+      <div className="main-body">
+      
+      <div classname="imgs">
+      <img src={Html} alt="Flowers in Chania"></img>
+      </div>
+
+      <div classname="body-text" >Habitasse ante nostra, elementum dignissim fermentum varius vulputate facilisis molestie per nascetur fames. Leo eleifend mauris senectus rutrum nisi molestie. Penatibus nunc tincidunt arcu sagittis conubia. Tempus porttitor phasellus dignissim lobortis non faucibus nisi lobortis nascetur ac parturient. Lobortis cum?
+</div>
+      
+      </div>
+</div>
 
     );
   }
--- src/Nav.js
+++ src/Nav.js
@@ -42,7 +42,7 @@ export default class Example extends React.Component {
               </NavItem>
               <NavItem>
 
-                <NavLink href="/components/">Contact</NavLink>
+                <NavLink href="https://www.google.com">Contact</NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
