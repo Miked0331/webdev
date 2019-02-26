c8cfa919eb108bede79545f4de971662f3446c85
miked0331 <mikedragun@gmail.com>
2019-02-26 13:19:29 -0500

    Updates

---
+++ precache-manifest.11c9a0e0c8a56b6a227ae924fe21dda6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbffc781ccd01e2605c272b9cfe6218",
+    "url": "/webdev/static/media/icon2.1dbffc78.png"
+  },
+  {
+    "revision": "6b58195739afcdc1f04138215f5ba872",
+    "url": "/webdev/static/media/html.6b581957.jpg"
+  },
+  {
+    "revision": "7849ddbe17a6cd08d59f",
+    "url": "/webdev/static/js/runtime~main.7849ddbe.js"
+  },
+  {
+    "revision": "c96aa6aea42feb829f7b",
+    "url": "/webdev/static/js/main.c96aa6ae.chunk.js"
+  },
+  {
+    "revision": "881f512d8b7b2a9732c9",
+    "url": "/webdev/static/js/2.881f512d.chunk.js"
+  },
+  {
+    "revision": "c96aa6aea42feb829f7b",
+    "url": "/webdev/static/css/main.7cb432a9.chunk.css"
+  },
+  {
+    "revision": "881f512d8b7b2a9732c9",
+    "url": "/webdev/static/css/2.cfef79e3.chunk.css"
+  },
+  {
+    "revision": "3e299fe715a8b099cc7ae70abdaa0733",
+    "url": "/webdev/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webdev/precache-manifest.8e278279a20792d66f6be2d8e78f9e32.js"
+  "/webdev/precache-manifest.11c9a0e0c8a56b6a227ae924fe21dda6.js"
 );
 
 workbox.clientsClaim();
