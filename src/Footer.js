307cb892b1e27a31bf5cda93f7d532327201c296
miked0331 <mikedragun@gmail.com>
2019-03-04 14:00:33 -0500

    footer changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import GitHub from './Image/github.png'
+import Linkedin from './Image/linkedin.png'
 
 class Footer extends Component {
     render() {
       return (
 <div className="main-footer">
 
-<div className="footer-text">Contact me @ mikedragun@gmail.com </div>
-
-<div className="footer-icon">
+<div className="footer-div">
 <a href="https://github.com/Miked0331" target="_blank" title="GitHub">
-<img src={GitHub} alt="Flowers in Chania"></img></a>
+<img className="footer-icon" src={GitHub} alt="Flowers in Chania"></img></a>
+</div>
+
+<div className="footer-div">Contact me @ mikedragun@gmail.com </div>
+
+<div className="footer-div">
+<a href="https://github.com/Miked0331" target="_blank" title="LinkedIn">
+<img className="footer-icon" src={Linkedin} alt="Flowers in Chania"></img></a>
 </div>
 
 </div>
       )}}
 
 
-      export default Footer
+export default Footer
